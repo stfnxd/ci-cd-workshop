0f249614fc3d90dd5b4c445952884fc4ed359072
stfn <steffenriis@outlook.com>
2024-05-01 23:13:46 +0200

    fix: Added connection for app.js to the API, aswell as updated the workflow for tests of api and web

--- web/app.js
+++ web/app.js
@@ -8,24 +8,24 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
+const apiUrl = 'http://api:3001/api/names'
 
-// TODO: You must change the connectionstring to the API when deploying in docker compose
+
 app.get('/', (req, res) => {
-fetch('http://localhost:3001/api/names')
-    .then(response => response.json())
-    .then(names => res.render('index', { names }))
-    .catch(err => res.status(500).json(err));
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(names => res.render('index', { names }))
+        .catch(err => res.status(500).json(err));
 });
 
-// TODO: You must change the connectionstring to the API when deploying in docker compose
 app.post('/add-name', (req, res) => {
-fetch('http://localhost:3001/api/names', {
-    method: 'POST',
-    body: JSON.stringify({ name: req.body.name }),
-    headers: { 'Content-Type': 'application/json' }
-})
-.then(response => res.redirect('/'))
-.catch(err => res.status(500).json(err));
+    fetch(apiUrl, {
+        method: 'POST',
+        body: JSON.stringify({ name: req.body.name }),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => res.redirect('/'))
+    .catch(err => res.status(500).json(err));
 });
 
 app.listen(port, () => {
